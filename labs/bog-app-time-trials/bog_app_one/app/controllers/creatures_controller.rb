b8ba8099c0f62552b04b0a583696d2b1dbb960e0
petercolella <peter.joseph.colella@gmail.com>
2017-09-06 12:28:22 -0400

    Part 2

--- labs/bog-app-time-trials/bog_app_one/app/controllers/creatures_controller.rb
+++ labs/bog-app-time-trials/bog_app_one/app/controllers/creatures_controller.rb
@@ -4,4 +4,21 @@ class CreaturesController < ApplicationController
 		@creatures = Creature.all
 	end
 
+	def new
+		@creature = Creature.new
+	end
+
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature = Creature.new(creature_params)
+
+		if creature.save
+			redirect_to creature_path(creature)
+		end
+	end
+
+	def show
+		@creature = Creature.find(params[:id])
+	end
+
 end
